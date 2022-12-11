07e12af373d38b601496a9ae829d7ee57dd6e7dd
rajesh <rajeshpgdav3444@gmail.com>
2022-12-11 09:58:15 +0530

    Add project files.

---
+++ JavaScriptPro/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $("#btn").click(function () {
+        $("#h1").css("color", "red")
+    })
+
+    $("#h1").hover(function(){
+        $("#h1").css("background-color", "yellow")
+
+    })
+    $("#h1").mouseout(function () {
+        $("#h1").css("background-color", "gray")
+
+    })
+    $("#btn").click(function () {
+        var txt = $("#txt").val()
+        if (txt.length < 3) {
+            alert("Please enter min 3 characters...")
+        }
+    })
+    $("#txt").keyup(function () {
+        var txt = $("#txt").val()
+        $("#txt").val(txt.toUpperCase())
+    })
+})
