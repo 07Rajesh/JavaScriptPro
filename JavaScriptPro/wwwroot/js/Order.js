7703b367b1559fe616920788ee57aa86f1b0c805
rajesh <rajeshpgdav3444@gmail.com>
2022-12-17 13:19:49 +0530

    Updated

---
+++ JavaScriptPro/wwwroot/js/Order.js
@@ -0,0 +1,63 @@
+﻿
+var menus = [
+    {"item":"Pasta","price":200},
+    {"item":"Burger","price":300},
+    {"item":"Pizza","price":350},
+    {"item":"Chease Pizza","price":390},
+    {"item":"Samosa","price":20},
+    {"item":"Paneer Samosa","price":30}
+]
+
+var customerItems = [];
+
+$(document).ready(function () {
+    bindMenuDDL()
+
+    $("#btnAdd").click(addOrder)
+
+})
+
+
+function addOrder() {
+    if ($("#ddlMenu").val == "" || $("#ddlMenu").val == "") {
+        alert("Please Select Menu")
+    }
+    else if ($("#txtQuantity").val() == "" || $("#txtQuantity").val() == "0") {
+        alert("Please enter Quantity")
+    }
+    else {
+        var menu = $("#ddlMenu option:selected").text()
+        var price = $("#ddlMenu").val()
+        var quantity = $("#txtQuantity").val()
+        var obj = { "item": menu, "price": price, "quantity": quantity, "totalprice": price * quantity }
+        customerItems.push(obj)
+
+        $("#ddlMenu").val("-1")
+        $("#txtQuantity").val("0")
+
+        bindOrder()
+    }
+}
+
+function bindOrder() {
+
+    var tbl = "<table class='table table-bordered table-hover'><tr class='table-dark'><th>SNO.</th><th>Menu</th><th>Price</th><th>Quantity</th><th>Total Price</th></tr>"
+
+    customerItems.forEach(function (item, index) {
+        tbl += "<tr><td>" + (index + 1) + "</td><td>" + item.item + "</td><td>" + item.price + "</td><td>" + item.quantity + "</td><td>" + item.totalprice + "</td></tr>"
+
+    })
+
+    tbl += "</table>"
+    $("#divOrder").html(tbl)
+}
+
+
+
+function bindMenuDDL() {
+    var op = "<option value='-1'>Select</option>"
+    menus.forEach(function (item) {
+        op += "<option value='" + item.price + "'>" + item.item + "</option >"
+    })
+    $("#ddlMenu").html (op)
+}
--- JavaScriptPro/wwwroot/js/site.js
+++ JavaScriptPro/wwwroot/js/site.js
@@ -1,8 +1,9 @@
-﻿$(document).ready(function () {
+﻿var items = [];
+$(document).ready(function () {
     $("#btn").click(function () {
         $("#h1").css("color", "red")
     })
-
+    /*
     $("#h1").hover(function(){
         $("#h1").css("background-color", "yellow")
 
@@ -11,14 +12,74 @@
         $("#h1").css("background-color", "gray")
 
     })
+    */
+
+  
     $("#btn").click(function () {
         var txt = $("#txt").val()
         if (txt.length < 3) {
             alert("Please enter min 3 characters...")
         }
+        else {
+
+            if (items.indexOf(txt) >= 0) {
+                alert("This items is already Exist!")
+            }
+            else {
+
+                if ($("#btn").text() == "Update") {
+                    items.splice($("#hdn").val(), 1, txt)
+                    $("#btn").text("Add")
+                }
+                else {
+
+                    items.push(txt);
+                  
+                }
+                printList(items)
+                $("#txt").val("")
+            }
+        }
     })
     $("#txt").keyup(function () {
-        var txt = $("#txt").val()
-        $("#txt").val(txt.toUpperCase())
+        var txt = $(this).val();
+        var sitems = items.filter(function (item) { return item.startsWith(txt) })
+        if (sitems.length == 0) {
+            sitems = items
+        }
+        printList(sitems);
     })
+
+
 })
+
+    function printList(items) {
+        var list = "<ol>"
+        items.forEach(function (item, index) {
+            list += "<li>" + item + "<a onclick='editItem(" + index +")'><span class='close'>edit</span></a><a onclick='removeItem(" + index + ")'><span class='close'>&times;</span></a></li>"
+        })
+
+
+
+        list + "</ol>"
+        $("#displayList").html(list)
+}
+function removeItem(index) {
+    items.splice(index, 1);
+    printList(items)
+
+}
+function editItem(index) {
+    $("#txt").val(items[index])
+    $("#btn").text("Update")
+    $("#hdn").val(index)
+}
+
+
+//    $("#txt").keyup(function () {
+//        var txt = $("#txt").val()
+//        $("#txt").val(txt.toUpperCase())
+//    })
+
+
+var students = {"name":"Aman", "age":15, "gender":"male"}
